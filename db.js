8811aff5300568a5d31fb4cb38f83321bab29578
Pacifique Clement Ndayisenga <pacifiqueclement@gmail.com>
2019-01-20 16:46:22 +0200

    signup endpoint

---
+++ db.js
@@ -0,0 +1,69 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+/**
+ * Create Tables
+ */
+const createTables = () => {
+  const usersQuery = `CREATE TABLE IF NOT EXISTS
+                      users(
+                        id SERIAL NOT NULL PRIMARY KEY,
+                        firstname VARCHAR(200) NOT NULL,
+                        lastname VARCHAR(200) NOT NULL,
+                        othername VARCHAR(200),
+                        email VARCHAR(200) NOT NULL UNIQUE,
+                        phone_number VARCHAR(200) NOT NULL,
+                        username VARCHAR(200) UNIQUE NOT NULL,
+                        password VARCHAR(200) NOT NULL,
+                        registered TIMESTAMP NOT NULL,
+                        is_admin BOOLEAN NOT NULL DEFAULT false
+                      )`;
+
+  pool.query(usersQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+/**
+ * Drop Tables
+ */
+const dropTables = () => {
+  const usersQuery = 'DROP TABLE IF EXISTS users';
+  pool.query(usersQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  createTables,
+  dropTables,
+};
+
+require('make-runnable');
