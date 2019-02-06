cd869294d82762a103972d240e11cdd24361b6e0
Pacifique Clement Ndayisenga <pacifiqueclement@gmail.com>
2019-02-06 14:37:45 +0200

    tests update

--- server/test/user.js
+++ server/test/user.js
@@ -1,11 +1,7 @@
 import chai from 'chai';
-
 import chaiHttp from 'chai-http';
-
 import moment from 'moment';
-
 import app from '../../app';
-
 import db from '../db/seeder';
 
 chai.should();
@@ -16,9 +12,7 @@ chai.use(require('chai-things'));
 describe('Questioner', () => {
   before((done) => {
     db.removeAllUsers();
-    db.removeAllMeetups();
     db.addUser();
-    db.addMeetup();
     done();
   });
 
@@ -42,12 +36,8 @@ describe('Questioner', () => {
           res.body.should.have.property('status').eql(201);
           res.body.should.have.property('data');
           res.body.data.should.be.a('array');
-          res.body.data.should.all.have.property('firstname', user.firstname);
-          res.body.data.should.all.have.property('lastname', user.lastname);
-          res.body.data.should.all.have.property('othername', user.othername);
-          res.body.data.should.all.have.property('email', user.email);
-          res.body.data.should.all.have.property('phone_number', user.phone_number);
-          res.body.data.should.all.have.property('username', user.username);
+          res.body.data.should.all.have.property('token');
+          res.body.data.should.all.have.property('user');
           done();
         });
     });
@@ -57,16 +47,6 @@ describe('Questioner', () => {
         username: 'Paccy10',
         password: 'password',
       };
-
-      const userResponse = {
-        firstname: 'Pacifique',
-        lastname: 'Ndayisenga',
-        othername: 'Clement',
-        email: 'pacifiqueclement@gmail.com',
-        phone_number: '0781983488',
-        username: 'Paccy10',
-        password: '$2a$10$9ErZa7Rw/OPHp1mLllOi1uK/3omWtjaagg.fZyquC3i11rn0WoKZ.',
-      };
       chai.request(app)
         .post('/api/v1/auth/login')
         .send(user)
@@ -75,12 +55,8 @@ describe('Questioner', () => {
           res.body.should.have.property('status').eql(200);
           res.body.should.have.property('data');
           res.body.data.should.be.a('array');
-          res.body.data.should.all.have.property('firstname', userResponse.firstname);
-          res.body.data.should.all.have.property('lastname', userResponse.lastname);
-          res.body.data.should.all.have.property('othername', userResponse.othername);
-          res.body.data.should.all.have.property('email', userResponse.email);
-          res.body.data.should.all.have.property('phone_number', userResponse.phone_number);
-          res.body.data.should.all.have.property('username', userResponse.username);
+          res.body.data.should.all.have.property('token');
+          res.body.data.should.all.have.property('user');
           done();
         });
     });
