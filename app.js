05967b16f8114e54cacf87a2380141928067870c
Pacifique Clement Ndayisenga <pacifiqueclement@gmail.com>
2019-02-15 17:30:30 +0200

    Access control allow methods

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH');
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
